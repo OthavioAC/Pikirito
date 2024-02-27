747828aa228c28604e3ff2a3c1c0ea57b94cb8c3
Othavio A. Cajarana <othaviocajarana@hotmail.com>
2024-02-27 19:39:10 -0300

    CodeDungeon 0.0.11
    Luz com iva feito

--- CodeDungeon/Assets/Scripts/TochLight.cs
+++ CodeDungeon/Assets/Scripts/TochLight.cs
@@ -11,8 +11,9 @@ public class TochLight : MonoBehaviour
     [SerializeField] private float lightMax = 1.0f;
     [SerializeField] private float OuterRadius = 2;
     [SerializeField] private float collightmax=10;
-    [SerializeField] private float noiseConstant = 20;
-    [SerializeField] private float intensityConstant = 0.5f; 
+    [SerializeField] private float noiseCoef = 20;
+    [SerializeField] private float intensityCoef = 0.5f;
+    [SerializeField] private float Offset = 3f;
     private float collight=0;
     float xpoint;
     
@@ -33,11 +34,11 @@ public class TochLight : MonoBehaviour
         }
         else
         {
-            float intensity =  Mathf.PerlinNoise(xpoint,Time.time*noiseConstant)+0.5f;
+            float intensity =  Mathf.PerlinNoise(xpoint,Time.time*noiseCoef);
             collight = 0;
             //float intensity = Random.Range(lightMin, lightMax);
-            GetComponent<Light2D>().pointLightOuterRadius = intensity*OuterRadius;
-            GetComponent<Light2D>().intensity = (intensity*intensityConstant)*0.85f;
+           // GetComponent<Light2D>().pointLightOuterRadius = intensity*OuterRadius;
+            GetComponent<Light2D>().intensity = (intensity*intensityCoef)+Offset;
             Debug.Log(intensity);
         }
     }
