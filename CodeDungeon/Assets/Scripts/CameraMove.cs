4ddf1e79a56e66b749985f5f947afc3535ff103c
Othavio A. Cajarana <47645796+OthavioAC@users.noreply.github.com>
2024-04-04 20:34:28 -0300

    Recomeço do projeto

---
+++ CodeDungeon/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour 
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private float zoomStep, minCamSize, maxCamSize;
+    [SerializeField] private float minSizeX, maxSizeX, minSizeY, maxSizeY;
+    [SerializeField] private GameObject Block;
+    private Vector3 dragOrigin;
+
+
+    private void Awake()
+    {
+        minSizeX = -5;
+        maxSizeX = 5;
+        minSizeY = -5;
+        maxSizeY = 5;
+
+    }
+    private void Update()
+    {
+        PanCamera();
+    }
+
+    private void PanCamera()
+    {
+        if (!Block.GetComponent<SideBlock>().GetBlocked())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 differ = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+                cam.transform.position = ClampCamera(cam.transform.position + differ);
+            }
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                ZoomIn();
+            }
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                ZoomOut();
+            }
+        }
+    }
+
+    public void ZoomIn()
+    {
+        float newSize = cam.orthographicSize - zoomStep;
+        cam.orthographicSize = Mathf.Clamp(newSize,minCamSize, maxCamSize);
+        cam.transform.position = ClampCamera(cam.transform.position);
+    }
+    public void ZoomOut()
+    {
+        float newSize = cam.orthographicSize + zoomStep;
+        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+        cam.transform.position = ClampCamera(cam.transform.position);
+    }
+
+    private Vector3 ClampCamera(Vector3 targetPosition)
+    {
+        float camHeight = cam.orthographicSize;
+        float camWidth = cam.orthographicSize * cam.aspect;
+
+        float minX = minSizeX + camWidth;
+        float maxX = maxSizeX - camWidth;
+        float minY = minSizeY + camHeight;
+        float maxY = maxSizeY - camHeight;
+
+        float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        return new Vector3(newX, newY,targetPosition.z);
+    }
+
+    public void Restart()
+    {
+        minSizeX = -5;
+        maxSizeX = 5;
+        minSizeY = -5;
+        maxSizeY = 5;
+    }
+}
