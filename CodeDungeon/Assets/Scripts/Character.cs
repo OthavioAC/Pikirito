fc14283c1991551e0a4b89ac1afabae5207319a6
Othavio A. Cajarana <47645796+OthavioAC@users.noreply.github.com>
2024-04-05 21:55:23 -0300

    Movimento

---
+++ CodeDungeon/Assets/Scripts/Character.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float velocidade = 5f; // Velocidade de movimento
+    public Vector2 limiteMin; // Canto inferior esquerdo da área
+    public Vector2 limiteMax; // Canto superior direito da área
+
+    public string state = "Idle";
+
+
+    private float idleTime = 0;
+    private float idleTimeMax = 4;
+    private Vector2 walkingTo = new Vector2(0,0);
+
+    private Vector2 destinoAleatorio;
+
+    private void Start()
+    {
+        transform.position = new Vector2(0, 0);
+    }
+
+    private void Update()
+    {
+        if (state == "Idle")
+        {
+            if (idleTime <= idleTimeMax)
+            {
+                idleTime += Time.deltaTime;
+            }
+            else
+            {
+                if (Vector2.Distance(transform.position, destinoAleatorio) < 0.1f)
+                {
+                    idleTime = 0;
+                    destinoAleatorio = PosRandom();
+                    print(destinoAleatorio);
+                }
+                else
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, destinoAleatorio, velocidade * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    private Vector2 PosRandom()
+    {
+        return new Vector2(Random.Range(limiteMin.x, limiteMax.x), Random.Range(limiteMin.y, limiteMax.y));
+    }
+}
