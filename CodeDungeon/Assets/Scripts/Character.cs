6d46e16fb10d2758e3c603ab74fc492a3071d9f6
Othavio A. Cajarana <47645796+OthavioAC@users.noreply.github.com>
2024-04-16 22:02:33 -0300

    Status adicionados

--- CodeDungeon/Assets/Scripts/Character.cs
+++ CodeDungeon/Assets/Scripts/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -31,19 +32,87 @@ public class Character : MonoBehaviour
     private string stat_Sujeira = "Limpo";
     private string stat_Energia = "Animado";
 
-    private int lastComida;
-    private int lastDiversao;
-    private int lastAgua;
-    private int lastSujeira;
-    private int lastEnergia;
+    private DateTime lastComida;
+    private DateTime lastDiversao;
+    private DateTime lastAgua;
+    private DateTime lastSujeira;
+    private DateTime lastEnergia;
 
     private void Start()
     {
         transform.position = new Vector2(0, 0);
+        if(lastComida.Year==1)
+        {
+            DateTime date = DateTime.Now;
+            lastComida = date.AddHours(-3);
+            lastDiversao = date.AddHours(-3);
+            lastAgua = date.AddHours(-3);
+            lastSujeira = date.AddHours(-3);
+            lastEnergia = date.AddHours(-3);
+        }
+
     }
 
     private void Update()
     {
+        DateTime dat = DateTime.Now;
+
+        DateTime datSemComida = dat.AddTicks(-lastComida.Ticks);
+        if (datSemComida.Hour < 3)
+        {
+            stat_Comida = "Gordo";
+        }
+        if (datSemComida.Hour >= 3)
+        {
+            stat_Comida = "Cheio";
+        }
+        if (datSemComida.Hour > 6)
+        {
+            stat_Comida = "Neutro";
+        }
+        if (datSemComida.Hour > 12)
+        {
+            stat_Comida = "Com Fome";
+        }
+        if (datSemComida.Hour > 24)
+        {
+            stat_Comida = "Esfomeado";
+        }
+        if (datSemComida.Hour > 72)
+        {
+            stat_Comida = "Morto - Fome";
+        }
+
+        DateTime datSemDiversao = dat.AddTicks(-lastDiversao.Ticks);
+        if (datSemDiversao.Hour < 3)
+        {
+            stat_Diversao = "Excitado";
+        }
+        if (datSemDiversao.Hour >= 3)
+        {
+            stat_Diversao = "Feliz";
+        }
+        if (datSemDiversao.Hour >= 8)
+        {
+            stat_Diversao = "Neutro";
+        }
+        if (datSemDiversao.Hour >= 16)
+        {
+            stat_Diversao = "Triste";
+        }
+        if (datSemDiversao.Hour >= 32)
+        {
+            stat_Diversao = "Depressivo";
+        }
+
+
+
+
+
+
+
+
+
         if (state == "Idle")
         {
             if (idleTime <= idleTimeMax)
